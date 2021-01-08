78b4f4b8a0cb967760724e8a7c849e974710246f
intrvertmichael <michaelpaguay@gmail.com>
2021-01-07 21:26:36 -0500

    log in form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,18 @@
 class RegistrationsController < ApplicationController
     def create
-        user = User.create!(user_params)
+        user = User.create!(
+            username: params["username"].downcase!,
+            password: params["password"],
+            password_confirmation: params["password_confirmation"],
+        )
 
         if user
             session[:user_id] = user.id
-            render json: {
-                status: :created,
-                user: user
-            }
+            # render json: { status: :created, user: user }
+            redirect_to root_path
         else
             render json: { status: 500 }
         end
 
-    end
-
-    private
-    def user_params
-        params.permit(:username, :password, :password_confirmation)
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,19 @@ class SessionsController < ApplicationController
 
     def create
         user = User
-                .find_by(username: params["user"]["username"])
-                .try(:authenticate, params["user"]["password"])
+                .find_by(username: params["username"].downcase)
+                .try(:authenticate, params["password"])
 
         if user
             session[:user_id] = user.id
+            @username = user.username
             render json: {
                 status: :created,
                 logged_in: true,
                 user: user
             }
+            # redirect_to root_path
+
         else
             render json: { status: 401 }
         end
