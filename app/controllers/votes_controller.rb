6ff159a1456e56fd26bf9439185a9c1965fc8b9f
intrvertmichael <michaelpaguay@gmail.com>
2021-01-11 21:48:33 -0500

    small tweaks

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,20 +3,25 @@ class VotesController < ApplicationController
         pid = params[:id]
         uid = session[:user_id]
         change = params[:change] == "up"? 1 : -1
-        @vote = Vote.find_by(post_id: pid, user_id:uid)
 
-        if @vote
-            if @vote.value == change
-                @vote.value = 0
-                @vote.save
+
+        if uid
+            @vote = Vote.find_by(post_id: pid, user_id:uid)
+
+            if @vote
+                if @vote.value == change
+                    @vote.value = 0
+                    @vote.save
+                else
+                    @vote.value = change
+                    @vote.save
+                end
             else
-                @vote.value = change
-                @vote.save
+                amount = params[:change] == "up"? 1 : -1
+                Vote.create(user_id: uid, post_id: pid, value: amount)
             end
-        else
-            Vote.create(user_id: uid, post_id: pid, value: params[:change])
-        end
 
-        redirect_to root_path
+            redirect_to root_path
+        end
     end
 end
