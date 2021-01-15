1276c68add08eef0f278763cd99af223b71f53f6
intrvertmichael <michaelpaguay@gmail.com>
2021-01-14 19:40:47 -0500

    Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
     def show
         if params[:author_id]
-            @author = User.find(params[:author_id])
+            user = params[:author_id]
         else
-            @author = User.find(session[:user_id])
+            user = session[:user_id]
         end
+
+        @author = User.find(user)
+
+        posts = Post.where(user_id: user).all
+
+        total = 0
+
+        posts.map { |n|
+            post = Post.find(n.id)
+            total += post.points
+        }
+
+        @karma = total
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
     validates_presence_of :username
     validates_uniqueness_of :username
 
+    has_many :posts
     has_many :votes
     has_many :comments
+    has_many :comment_votes
 
+
+    def post_amount
+        self.posts.count
+    end
+
+    def comment_amount
+        self.comments.count
+    end
 end
