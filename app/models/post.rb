4b8e63e202548d1cffa1150a96afc74e91c4313c
intrvertmichael <michaelpaguay@gmail.com>
2021-01-20 10:01:08 -0500

    Regex to make sure Url is a valid link

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,10 @@ class Post < ApplicationRecord
   has_many :comments
 
 
-  validates_length_of :url, minimum: 5
   validates_length_of :title, minimum: 5
+  validates_length_of :url, minimum: 5
+
+  validates_format_of :url, :with => /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
 
   def points
     self.votes.sum(&:value)
